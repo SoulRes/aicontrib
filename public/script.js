de55f25485b0c566707edf445ea02e37ab3ca335
SoulRes <SoulResolut@mailfence.com>
2024-12-14 03:59:23 +0200

    Your commit message

--- public/script.js
+++ public/script.js
@@ -433,6 +433,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Function to handle payment
     async function processPayment(priceAmount, priceCurrency, paymentMethod, orderId) {
+        const referralCode = referralInput.value.trim(); // Get referral code from input
         try {
             console.log('Sending payment creation request with the following details:', {
                 price: priceAmount,
