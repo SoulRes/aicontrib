1ee04fb5ed4ab5049ebc063913c038597ff2b418
SoulRes <SoulResolut@mailfence.com>
2025-03-21 01:31:33 +0800

    Your commit message

--- public/script.js
+++ public/script.js
@@ -159,10 +159,10 @@ document.addEventListener("DOMContentLoaded", function () {
 
     const earningsElement = document.getElementById("earnings-counter");
     const earningsPerSecond = 1; // Change this value if needed
-    const startTimestamp = new Date("March 15, 2024 00:00:00 UTC").getTime() / 1000;
+    const startTimestamp = new Date("March 15, 2024 00:00:00 UTC").getTime() / 1500;
 
     function updateEarnings() {
-        const currentTime = Math.floor(Date.now() / 1000);
+        const currentTime = Math.floor(Date.now() / 1500);
         const timeElapsed = currentTime - startTimestamp;
         const currentEarnings = timeElapsed * earningsPerSecond;
 
